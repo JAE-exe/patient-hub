8328bebd7bce4491072e8bc097c9d220b929a5b0
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-07-16 23:14:24 +0530

    tailwind css setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased")}>
+      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased",font.variable)}>
         {children}
       </body>
     </html>
