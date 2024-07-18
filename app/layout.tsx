258625c28732eec07a5c28a07fe67062dca85060
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-07-19 00:58:08 +0530

    form done and assets added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const font = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -21,8 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased",font.variable)}>
-        {children}
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          font.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Hello World</h1>
-    </div>
+      <div className="flex h-screen max-h-screen">
+        <section className="remove-scrollbar container my-auto">
+          <div className="sub-container max-w-[496px]">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              alt="patient-hub"
+              width={1000}
+              height={1000}
+              className="mb-12 h-10 w-fit"
+            />
+            <PatientForm></PatientForm>
+            <div className="text-14-regular">
+              <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Patient Hub</p>
+              <Link href={"/?admin=true" } className="text-green-500"></Link>
+            </div>
+          </div>
+        </section>
+        <Image
+              src="/assets/images/onboarding-img.png"
+              alt="patient"
+              width={1000}
+              height={1000}
+              className="side-img max-w-[50%]"
+            />
+      </div>
   );
 }
